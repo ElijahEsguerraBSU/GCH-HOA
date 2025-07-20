42d20e3a0ae290524e20a298bde8848dda28f6c0
ElijahEsguerraBSU <22-07317@g.batstate-u.edu.ph>
2025-07-20 12:56:56 +0800

    Fixed dashboard page

--- lib/dashboard_page.dart
+++ lib/dashboard_page.dart
@@ -188,7 +188,7 @@ class _DashboardPageState extends State<DashboardPage> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     const Text(
-                      "Your Community Dashboard Branch Test",
+                      "Your Community Dashboard",
                       style: TextStyle(
                         fontSize: 22,
                         fontWeight: FontWeight.bold,
